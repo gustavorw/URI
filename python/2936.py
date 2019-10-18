4ed408e940bdc24933097be03b288d8f8f0fc0b6
DevGustavo <46036174+GustavoDev17@users.noreply.github.com>
2019-10-18 16:39:03 -0300

    add soluctions

---
+++ python/2936.py
@@ -0,0 +1,9 @@
+curupira = int(input())
+boitata = int(input())
+boto = int(input())
+mapinguari = int(input())
+lara = int(input())
+
+total = 225 + (curupira * 300) + (boitata *1500) + (boto * 600) + (mapinguari * 1000)+(lara*150)
+
+print(total)
