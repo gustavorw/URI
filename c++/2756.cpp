2ed39a8d640c41ba17c4384e7fc2081bb15b33a9
gustavo <gustavorw03@gmail.com>
2020-05-05 21:11:44 -0300

    add questions c++

---
+++ c++/2756.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(){
+    std::cout<<"       A\n";
+    std::cout<<"      B B\n";
+    std::cout<<"     C   C\n";
+    std::cout<<"    D     D\n";
+    std::cout<<"   E       E\n";
+    std::cout<<"    D     D\n";
+    std::cout<<"     C   C\n";
+    std::cout<<"      B B\n";
+    std::cout<<"       A\n";
+    return 0;
+}
